a5964d9b34b91f2dcb48bf0b8ce88f9953318c6a
YongSheng109 <93895098+YongSheng109@users.noreply.github.com>
2022-01-08 21:30:36 +0800

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 printf "Installing Linux RDP Please Wait... " >&2
 {
-sudo useradd -m CuteWolfie
-sudo adduser CuteWolfie sudo
-echo 'CuteWolfie:103465' | sudo chpasswd
-sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo useradd -m PingBypass
+sudo adduser PingBypass sudo
+echo 'PingBypass:123456' | sudo chpasswd
+sudo sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
@@ -18,13 +18,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser CuteWolfie chrome-remote-desktop
+sudo adduser PingBypass chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - CuteWolfie -c """$CRP"""
+su - PingBypass -c """$CRP"""
 printf 'Access Your RDP At https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
